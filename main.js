1edc16732963d7acfb9bac48237724f8b6b83a88
Emanoel Antonio <dev.emanoel@protonmail.com>
2021-01-14 11:47:12 -0300

    criar cobrinha

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-let box = 32;
-
-function criarBG() {
-    context.fillStyle = "Lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+let box = 32; // 32 px cada quadrado
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
 }
 
-criarBG();
+function createBackground() {
+    context.fillStyle = "Lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box); // x, y, width, height
+}
+
+function createSnake() {
+    for (i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+        
+    }
+}
+
+createBackground();
+createSnake();
