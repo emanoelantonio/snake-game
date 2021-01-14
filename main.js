ccb64738c3438b9978883f5d2c6ac53fb637b852
Emanoel Antonio <dev.emanoel@protonmail.com>
2021-01-14 19:03:42 -0300

    criar comida

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function createBackground() {
     context.fillStyle = "Lightgreen";
@@ -19,6 +23,11 @@ function createSnake() {
         context.fillRect(snake[i].x, snake[i].y, box, box);
         
     }
+}
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -39,6 +48,7 @@ function startGame() {
 
     createBackground();
     createSnake();
+    drawFood();
     
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
